7407c250e22d31b0337ad05386be56b81c643246
Chlerry <dannierli@outlook.com>
2020-04-23 23:34:19 -0700

    Add coarse16_train method

--- coarse_train.py
+++ coarse_train.py
@@ -1,6 +1,9 @@
 import helper.psnr as psnr
 import helper.load_imgs as load_imgs
 
+def coarse16_train(f_start,f_end,folder):
+    images =  load_imgs(folder, f_start, f_end)  
+
 if __name__ == "__main__":   
     folder = './dataset/BlowingBubbles_416x240_50/'
     b = 16 # blk_size
