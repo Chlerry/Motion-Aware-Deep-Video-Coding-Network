c26c9047a2fbcaf0b0a44e3e989ac5ba077dfb50
Chlerry <dannierli@outlook.com>
2020-04-23 23:28:55 -0700

    Add psnr and load_imgs

---
+++ helper.py
@@ -0,0 +1,15 @@
+def psnr(img_true, img_recovered):    
+    pixel_max = 255.0
+    mse = np.mean((img_true-img_recovered)**2)
+    p = 20 * math.log10( pixel_max / math.sqrt( mse ))
+    return p
+
+def load_imgs(path, start, end):
+    train_set = []
+    for n in range(start, end):
+        fname = path +  str(n) + ".png"
+        img = cv2.imread(fname, 1)
+        if img is not None:
+                train_set.append(img)
+    train_set = np.array(train_set)
+    return train_set
