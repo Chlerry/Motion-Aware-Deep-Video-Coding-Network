de67e963727572a2009207bb86580b7dbe080b6c
Chlerry <dannierli@outlook.com>
2020-04-25 04:24:20 -0700

    Replace code blocks with existing module

--- prediction_inference.py
+++ prediction_inference.py
@@ -23,22 +23,7 @@ from keras.callbacks import ModelCheckpoint
 from keras.callbacks import EarlyStopping
 
 
-def psnr(img_true, img_recovered):    
-    pixel_max = 255.0
-    mse = np.mean((img_true-img_recovered)**2)
-    p = 20 * math.log10( pixel_max / math.sqrt( mse ))
-    return p
-
-
-def load_imgs(path, start, end):
-    train_set = []
-    for n in range(start, end):
-        fname = path  + str(n) + ".png"
-        img = cv2.imread(fname, 1)
-        if img is not None:
-                train_set.append(img)
-    train_set = np.array(train_set)
-    return train_set
+from helper import psnr, load_imgs
 
 def coarse16_inference(folder, start, end):
     images =  load_imgs(folder, start, end)
