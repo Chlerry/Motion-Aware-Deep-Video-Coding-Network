455ca150552f6e1a995b9b109b48441d4a152599
Chlerry <dannierli@outlook.com>
2020-04-25 01:09:25 -0700

    Repalce code blocks with existing module

--- prediction-train.py
+++ prediction-train.py
@@ -22,22 +22,7 @@ from keras.optimizers import Adam
 from keras.callbacks import ModelCheckpoint
 from keras.callbacks import EarlyStopping
 
-def psnr(img_true, img_recovered):    
-    pixel_max = 255.0
-    mse = np.mean((img_true-img_recovered)**2)
-    p = 20 * math.log10( pixel_max / math.sqrt( mse ))
-    return p
-
-
-def load_imgs(path, start, end):
-    train_set = []
-    for n in range(start, end):
-        fname = path  + str(n) + ".png"
-        img = cv2.imread(fname, 1)
-        if img is not None:
-                train_set.append(img)
-    train_set = np.array(train_set)
-    return train_set
+from helper import psnr, load_imgs
 
 def coarse16_pred(folder, start, end): # inference: compression net, compression ratio = 1/16
     images =  load_imgs(folder, start, end)
