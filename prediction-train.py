003ece6c67e993d526372cbf90654ba69536f085
Chlerry <dannierli@outlook.com>
2020-04-25 03:20:23 -0700

    Repalce code blocks with existing module

--- prediction-train.py
+++ prediction-train.py
@@ -23,51 +23,16 @@ from keras.callbacks import ModelCheckpoint
 from keras.callbacks import EarlyStopping
 
 from helper import psnr, load_imgs
-
-def coarse16_pred(folder, start, end): # inference: compression net, compression ratio = 1/16
-    images =  load_imgs(folder, start, end)
-    
-    print(images.shape)
-    
-    #images =  load_imgs(folder, test_start,test_end)
-    
-    from keras.models import model_from_json
-    json_file = open('./models/BlowingBubbles_416x240_50_coarse16.json', 'r')
-    loaded_model_json = json_file.read()
-    json_file.close()
-    coarse_model = model_from_json(loaded_model_json)
-    # load weights into new model
-    coarse_model.load_weights("./models/BlowingBubbles_416x240_50_coarse16.hdf5")
-    print("Loaded model from disk")
-    
-    # evaluate loaded model on test data
-    coarse_model.compile(optimizer='adam', loss='mse', metrics=['acc'])
-    
-    
-    # prepare image blocks: bxbx3
-    coarse_set = []
-    for img in images:
-        block = []
-        for y in range(0, img.shape[0], b):
-            for x in range(0, img.shape[1], b):
-                block = img[y:y + b, x:x + b]
-                block = block.reshape(b*b, 3)
-                coarse_set.append(block)
-    
-    coarse_set = np.array(coarse_set)
-    coarse_set2 = coarse_set.reshape(coarse_set.shape[0], b, b, 3)
-    
-    coarse_frames = coarse_model.predict(coarse_set2) # inference: compression/decompression
-
-    return coarse_frames
+from coarse_test import coarse16_test
 
 def pred_train(folder, start, end, b, bm):
     
     N_frames = end - start
     
     # generate the decoded frames from the compression net
-    coarse_frames = coarse16_pred(folder, start, end)
     images = load_imgs(folder, start, end)
+    coarse_frames = coarse16_test(images, b)
+    
     width, height = images.shape[1], images.shape[2]
 
     N_blocks = int((width*height)/(b*b))
