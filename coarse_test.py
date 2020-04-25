6c2a9c7fcc27ac3a79505a30b2bc7f2ad6c3e895
Chlerry <dannierli@outlook.com>
2020-04-24 23:17:54 -0700

    Replace psnr and load_imgs method with existing module

--- coarse_test.py
+++ coarse_test.py
@@ -18,22 +18,8 @@ from keras.callbacks import EarlyStopping
 
 SEED=42
 
-def psnr(img_true, img_recovered):    
-    pixel_max = 255.0
-    mse = np.mean((img_true-img_recovered)**2)
-    p = 20 * math.log10( pixel_max / math.sqrt( mse ))
-    return p
-
-
-def load_imgs(path, start, end):
-    train_set = []
-    for n in range(start, end):
-        fname = path +  str(n) + ".png"
-        img = cv2.imread(fname, 1)
-        if img is not None:
-                train_set.append(img)
-    train_set = np.array(train_set)
-    return train_set
+from helper import psnr as psnr
+from helper import load_imgs 
 
 def results(coarse_frames, images, foldername,  test_start, test_end, b):
     
