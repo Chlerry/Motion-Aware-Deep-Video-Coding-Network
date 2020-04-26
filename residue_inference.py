e2df85ecf12dec9793dfc48ba00657e1d6cc0717
Chlerry <dannierli@outlook.com>
2020-04-25 22:16:26 -0700

    Replace code blocks with existing module

--- residue_inference.py
+++ residue_inference.py
@@ -25,140 +25,8 @@ from keras.callbacks import EarlyStopping
 
 from helper import psnr, load_imgs
 from coarse_test import coarse16_test
-
-def pred_inference(folder, start, end, b, bm, coarse_frames):
-    
-    N_frames = end - start
-        
-    #coarse_frames = coarse16_pred(folder, start, end)
-    images = load_imgs(folder, start, end)
-    width, height = images.shape[1], images.shape[2]
-
-    N_blocks = int((width*height)/(b*b))
-    
-    decoded = []
-    f = coarse_frames.reshape(N_frames, N_blocks, b*b, 3)
-    for n in f:
-        result = np.zeros((width, height, 3))
-        i = 0
-        for y in range(0, result.shape[0], b):
-           for x in range(0, result.shape[1], b):
-               res = n[i].reshape(b,b, 3)
-               result[y:y + b, x:x + b] = res
-               i = i + 1
-              
-        decoded.append(result)
-    
-    decoded = np.array(decoded) # re-group the decoded frames
-    
-    prev = []
-    
-    for i in range(0, N_frames-2): # take the reference frames
-        img = decoded[i] # frame the decoded frames
-        
-        for y in range(0, img.shape[0], bm):
-            for x in range(0, img.shape[1], bm):
-                block = np.zeros((b, b, 3))
-                if (y + b) >= img.shape[0] and (x + b) < img.shape[1] :
-                    block = img[y-bm:y-bm+b, x:x+b]
-                    block = block.reshape(b*b,3)
-                    prev.append(block)
-                elif (x + b) >= img.shape[1] and (y + b) < img.shape[0]:
-                    block = img[y:y+b, x-bm:x-bm+b]
-                    block = block.reshape(b*b, 3)
-                    prev.append(block)
-                elif (y + b) >= img.shape[0] and (x + b) >= img.shape[1] :
-                    block = img[y-bm:y-bm+b, x-bm:x-bm+b]
-                    block = block.reshape(b*b, 3)
-                    prev.append(block)
-                else:
-                    block = img[y:y + b, x:x + b]
-                    block = block.reshape(b*b, 3)
-                    prev.append(block)
-    
-                
-    prev = np.array(prev)
-    prev = prev.reshape(prev.shape[0], b, b, 3)
-    print(prev.shape)
-    
-    B = []
-    
-    for i in range(0, N_frames-2): 
-        next_f = decoded[i+2]
-        
-        for y in range(0, img.shape[0], bm):
-            for x in range(0, img.shape[1], bm):
-                if (y + b) >= img.shape[0] and (x + b) < img.shape[1] :
-                    block = next_f[y-bm:y-bm+b, x:x+b]
-                    block = block.reshape(b*b,3)
-                    B.append(block)
-                elif (x + b) >= img.shape[1] and (y + b) < img.shape[0]:
-                    block = next_f[y:y+b, x-bm:x-bm+b]
-                    block = block.reshape(b*b, 3)
-                    B.append(block)
-                elif (y + b) >= img.shape[0] and (x + b) >= img.shape[1] :
-                    block = next_f[y-bm:y-bm+b, x-bm:x-bm+b]
-                    block = block.reshape(b*b, 3)
-                    B.append(block)
-                else:
-                    block = next_f[y:y + b, x:x + b]
-                    block = block.reshape(b*b, 3)
-                    B.append(block)
-    
-                
-    B = np.array(B)
-    B = B.reshape(B.shape[0], b, b, 3)
-    print(B.shape)
-    
-    C = []
-    
-    for i in range(0, N_frames-2): 
-        current = images[i+1]
-        for y in range(0, img.shape[0], bm):
-            for x in range(0, img.shape[1], bm):
-                block = current[y:y + bm, x:x + bm]
-                block = block.reshape(bm*bm, 3)
-                C.append(block)
-    
-    C = np.array(C)
-    C = C.reshape(C.shape[0], bm, bm, 3)
-    print(C.shape)
-    
-    # ============== YL: load model ===============================
-    
-    from keras.models import model_from_json
-    json_file = open('./models/BlowingBubbles_416x240_50_pred16.json', 'r')
-    loaded_model_json = json_file.read()
-    json_file.close()
-    pred_model = model_from_json(loaded_model_json)
-    # load weights into new model
-    pred_model.load_weights("./models/BlowingBubbles_416x240_50_pred16.hdf5")
-    print("Loaded model from disk")
-    
-    # evaluate loaded model on test data
-    pred_model.compile(optimizer='adam', loss='mse', metrics=['acc'])
-
-    predicted_frames = pred_model.predict([prev, B])
-    #final_prediction = np.zeros((N_frames-2,width,height,3))
-    final_prediction=[]
-    
-    #f = coarse_frames.reshape(N_frames, N_blocks, b*b, 3)
-    i=0
-    for n in range(N_frames-2):
-        result = np.zeros((width, height, 3))
-        
-        for y in range(0, width, bm):
-           for x in range(0, height, bm):
-               res = x
-               result[y:y + bm, x:x + bm,:] = predicted_frames[i].reshape(bm,bm,3)
-               i = i + 1
-              
-        final_prediction.append(result)
-    
-    final_prediction = np.array(final_prediction) # re-group the decoded frames
-        
-    return final_prediction
-    # ===================================================
+from prediction_inference import pred_inference
+from residue_train import regroup
 
 def performance_evaluation(folder,start,end,finalpred):
     images =  load_imgs(folder, start+1, end-1)
@@ -295,12 +163,14 @@ if __name__ == "__main__":
     test_start, test_end = 100, 120
     
     images =  load_imgs(folder, test_start, test_end)
-    
     coarse_frames = coarse16_test(images, b)
+
     bm = 8 # target block size to predict
-    predicted_frames = pred_inference(folder, test_start, test_end, b, bm, coarse_frames)
-    pred_amse, pred_apsnr, pred_assim = performance_evaluation(folder,test_start,test_end,predicted_frames)
+    N_frames = test_end - test_start
 
-    final_frames = residue_inference(folder, test_start, test_end, predicted_frames, folder_save)
+    predicted_frames = pred_inference(N_frames, b, bm, images, coarse_frames)
+    final_prediction = regroup(N_frames, images.shape, bm, predicted_frames)
+    pred_amse, pred_apsnr, pred_assim = performance_evaluation(folder,test_start,test_end,final_prediction)
+
+    final_frames = residue_inference(folder, test_start, test_end, final_prediction, folder_save)
     final_amse, final_apsnr, final_assim = performance_evaluation(folder,test_start,test_end,final_frames)
-
