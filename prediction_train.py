c746b79fcc9798fbd0ca6d0973d703c57c7b45e7
Chlerry <dannierli@outlook.com>
2020-04-26 00:29:55 -0700

    Update motion compention algorithm for pred_train

--- prediction_train.py
+++ prediction_train.py
@@ -43,7 +43,6 @@ def pred_train(folder, start, end, b, bm):
     
     N_frames = end - start
     
-    # generate the decoded frames from the compression net
     images = load_imgs(folder, start, end)
     coarse_frames = coarse16_test(images, b)
     
@@ -66,29 +65,56 @@ def pred_train(folder, start, end, b, bm):
     
     decoded = np.array(decoded) # re-group the decoded frames
     
-    # F'_t-1
     prev = []
     
-    for i in range(0, N_frames-2): # take the reference bxbx3 blocks
-        img = decoded[i] # from the decoded frames
+    for i in range(0, N_frames-2): # take the reference frames
+        img = decoded[i] # frame the decoded frames
         
         for y in range(0, img.shape[0], bm):
             for x in range(0, img.shape[1], bm):
-                block = np.zeros((b, b, 3))
-                if (y + b) >= img.shape[0] and (x + b) < img.shape[1] : # target blk: bottom row
-                    block = img[y-bm:y-bm+b, x:x+b]
+                block = np.zeros((b, b, 3))              
+                # bottom L
+                if (y + bm/2+b/2) >= img.shape[0] and (x+bm/2 - b/2) < 0 :
+                    block = img[y+bm-b:y+bm, x:x+b]
+                    block = block.reshape(b*b,3)
+                    prev.append(block)               
+                # bottom R
+                elif (y + bm/2+b/2) >= img.shape[0] and (x+bm/2 + b/2) >= img.shape[1] :
+                    block = img[y+bm-b:y+bm, x+bm-b:x+bm]
                     block = block.reshape(b*b,3)
                     prev.append(block)
-                elif (x + b) >= img.shape[1] and (y + b) < img.shape[0]: # target blk: right-most column
-                    block = img[y:y+b, x-bm:x-bm+b]
-                    block = block.reshape(b*b, 3)
-                    prev.append(block)
-                elif (y + b) >= img.shape[0] and (x + b) >= img.shape[1] : # target blk: bottom-right blk
-                    block = img[y-bm:y-bm+b, x-bm:x-bm+b]
-                    block = block.reshape(b*b, 3)
-                    prev.append(block)
-                else:
-                    block = img[y:y + b, x:x + b] # target blk: other positions
+                # top L
+                elif (y + bm/2-b/2) < 0 and (x+bm/2 - b/2) < 0 :
+                    block = img[y:y+b, x:x+b]
+                    block = block.reshape(b*b,3)
+                    prev.append(block)                    
+                # top R
+                elif (y + bm/2-b/2) < 0 and (x+bm/2 + b/2) >= img.shape[1] :
+                    block = img[y:y+b, x+bm-b:x+bm]
+                    block = block.reshape(b*b,3)
+                    prev.append(block)            
+                # top row
+                elif (y + bm/2-b/2) < 0 :
+                    block = img[y:y+b, int(x+bm/2-b/2):int(x+bm/2+b/2)]
+                    block = block.reshape(b*b,3)
+                    prev.append(block)                    
+                # bottom row
+                elif (y + bm/2+b/2) >= img.shape[0] :
+                    block = img[y+bm-b:y+bm, int(x+bm/2-b/2):int(x+bm/2+b/2)]
+                    block = block.reshape(b*b,3)
+                    prev.append(block)                    
+                # left column
+                elif (x+bm/2 - b/2) < 0 :
+                    block = img[int(y+bm/2-b/2):int(y+bm/2+b/2), x:x+b]
+                    block = block.reshape(b*b,3)
+                    prev.append(block)                    
+                # right column
+                elif (x+bm/2 + b/2) >= img.shape[1] :
+                    block = img[int(y+bm/2-b/2):int(y+bm/2+b/2), x+bm-b:x+bm]
+                    block = block.reshape(b*b,3)
+                    prev.append(block)                   
+                else: # normal
+                    block = img[int(y+bm/2-b/2):int(y+bm/2+b/2), int(x+bm/2-b/2):int(x+bm/2+b/2)]
                     block = block.reshape(b*b, 3)
                     prev.append(block)
     
@@ -97,41 +123,70 @@ def pred_train(folder, start, end, b, bm):
     prev = prev.reshape(prev.shape[0], b, b, 3)
     print(prev.shape)
     
-    # F'_t+1
     B = []
     
     for i in range(0, N_frames-2): 
         next_f = decoded[i+2]
-        
+    
+        # yL: new
         for y in range(0, img.shape[0], bm):
             for x in range(0, img.shape[1], bm):
-                if (y + b) >= img.shape[0] and (x + b) < img.shape[1] :
-                    block = next_f[y-bm:y-bm+b, x:x+b]
+                #block = np.zeros((b, b, 3))              
+                # bottom L
+                if (y + bm/2+b/2) >= img.shape[0] and (x+bm/2 - b/2) < 0 :
+                    block = next_f[y+bm-b:y+bm, x:x+b]
+                    block = block.reshape(b*b,3)
+                    B.append(block)               
+                # bottom R
+                elif (y + bm/2+b/2) >= img.shape[0] and (x+bm/2 + b/2) >= img.shape[1] :
+                    block = next_f[y+bm-b:y+bm, x+bm-b:x+bm]
                     block = block.reshape(b*b,3)
                     B.append(block)
-                elif (x + b) >= img.shape[1] and (y + b) < img.shape[0]:
-                    block = next_f[y:y+b, x-bm:x-bm+b]
+                # top L
+                elif (y + bm/2-b/2) < 0 and (x+bm/2 - b/2) < 0 :
+                    block = next_f[y:y+b, x:x+b]
+                    block = block.reshape(b*b,3)
+                    B.append(block)                    
+                # top R
+                elif (y + bm/2-b/2) < 0 and (x+bm/2 + b/2) >= img.shape[1] :
+                    block = next_f[y:y+b, x+bm-b:x+bm]
+                    block = block.reshape(b*b,3)
+                    B.append(block)            
+                # top row
+                elif (y + bm/2-b/2) < 0 :
+                    block = next_f[y:y+b, int(x+bm/2-b/2):int(x+bm/2+b/2)]
+                    block = block.reshape(b*b,3)
+                    B.append(block)                    
+                # bottom row
+                elif (y + bm/2+b/2) >= img.shape[0] :
+                    block = next_f[y+bm-b:y+bm, int(x+bm/2-b/2):int(x+bm/2+b/2)]
+                    block = block.reshape(b*b,3)
+                    B.append(block)                    
+                # left column
+                elif (x+bm/2 - b/2) < 0 :
+                    block = next_f[int(y+bm/2-b/2):int(y+bm/2+b/2), x:x+b]
+                    block = block.reshape(b*b,3)
+                    B.append(block)                    
+                # right column
+                elif (x+bm/2 + b/2) >= img.shape[1] :
+                    block = next_f[int(y+bm/2-b/2):int(y+bm/2+b/2), x+bm-b:x+bm]
+                    block = block.reshape(b*b,3)
+                    B.append(block)                   
+                else: # normal
+                    block = next_f[int(y+bm/2-b/2):int(y+bm/2+b/2), int(x+bm/2-b/2):int(x+bm/2+b/2)]
                     block = block.reshape(b*b, 3)
                     B.append(block)
-                elif (y + b) >= img.shape[0] and (x + b) >= img.shape[1] :
-                    block = next_f[y-bm:y-bm+b, x-bm:x-bm+b]
-                    block = block.reshape(b*b, 3)
-                    B.append(block)
-                else:
-                    block = next_f[y:y + b, x:x + b]
-                    block = block.reshape(b*b, 3)
-                    B.append(block)
+    
     
                 
     B = np.array(B)
     B = B.reshape(B.shape[0], b, b, 3)
     print(B.shape)
     
-    # F_t (using original frame as the target)
     C = []
     
     for i in range(0, N_frames-2): 
-        current = images[i+1] # 1, 3, 5, 7 ...
+        current = images[i+1]
         for y in range(0, img.shape[0], bm):
             for x in range(0, img.shape[1], bm):
                 block = current[y:y + bm, x:x + bm]
@@ -142,11 +197,7 @@ def pred_train(folder, start, end, b, bm):
     C = C.reshape(C.shape[0], bm, bm, 3)
     print(C.shape)
     
-    # Daniel: 
-    # 0, 2 -> 1
-    # 1, 3 -> 2
 
-    # construct the prediction net
     input1 = Input(shape = (b, b, 3))
     
     y = Conv2D(8, kernel_size=(5, 5), padding = "SAME", strides = 2, activation='relu')(input1)
@@ -165,7 +216,6 @@ def pred_train(folder, start, end, b, bm):
     x = Conv2D(128, kernel_size=(5, 5), padding = "SAME", strides = 1, activation='relu')(x)
     x = Model(inputs = input2, outputs = x)
     
-    # Fusion Module
     c = keras.layers.concatenate([y.output, x.output])
     
     z = Conv2D(128, kernel_size=(5, 5), padding = "SAME", strides = 1, activation='relu')(c)
@@ -184,19 +234,21 @@ def pred_train(folder, start, end, b, bm):
    # from keras.models import model_from_json
     # serialize model to JSON
     model_json = pred_model.to_json()
-    with open("./models/BlowingBubbles_416x240_50_pred16.json", "w") as json_file:
+    with open("./models/BlowingBubbles_416x240_50_pred16_0411.json", "w") as json_file:
         json_file.write(model_json)
+
     
     # define early stopping callback
     earlystop = EarlyStopping(monitor='val_loss', min_delta=2, patience=10, \
                               verbose=2, mode='auto', \
                               baseline=None, restore_best_weights=True)                    
     # define modelcheckpoint callback
-    checkpointer = ModelCheckpoint(filepath='./models/BlowingBubbles_416x240_50_pred16.hdf5',\
+    checkpointer = ModelCheckpoint(filepath='./models/BlowingBubbles_416x240_50_pred16_0411.hdf5',\
                                    monitor='val_loss',save_best_only=True)
     callbacks_list = [earlystop, checkpointer]
     pred_model.fit([prev, B], C, batch_size=10, epochs=5, verbose=2, validation_split=0.2, callbacks=callbacks_list)
     # ===================================================
+
     
 if __name__ == "__main__":   
     folder = './dataset/BlowingBubbles_416x240_50/'
