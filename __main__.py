be4296356ddb455b210f3a14e5a24fed51adea63
Chlerry <dannierli@outlook.com>
2020-06-26 13:26:03 -0700

    Remove prediction b1 b23 model files

--- __main__.py
+++ __main__.py
@@ -6,45 +6,38 @@ import coarse.train, coarse.test
 import prediction.train, prediction.inference
 import prediction.b1_train, prediction.b1_inference
 import prediction.b23_train, prediction.b23_inference
-import prediction.train_gnet5_new
 
 import residue.train, residue.inference
 import residue.b1_train, residue.b23_train
 import residue.b_inference
 
 if __name__ == "__main__":  
-################## Original Method: train #################
-    # # Train compression net
-    # coarse.train.main()
+# ################## Original Method: train #################
+#     # Train compression net
+#     coarse.train.main()
     
-    # # Train prediction net
-    # prediction.train.main()
+#     # Train prediction net
+#     prediction.train.main()
     
-    # # Train residue net
-    # residue.train.main()
+#     # Train residue net
+#     residue.train.main()
     
-################## Original Method: test ##################
-    # Test compression net
-    coarse.test.main()
+# ################## Original Method: test ##################
+#     # Test compression net
+#     coarse.test.main()
     
-    # Test prediction net
-    prediction.inference.main()
+#     # Test prediction net
+#     prediction.inference.main()
     
-    # Test residue net
-    residue.inference.main()
+#     # Test residue net
+#     residue.inference.main()
     
-##################### New Method: train ###################
-    # Train b1's prediction net
-    prediction.b1_train.main()
+# ##################### New Method: train ###################    
+#     # Train b1's residue net
+#     residue.b1_train.main()
     
-    # Train b1's residue net
-    residue.b1_train.main()
-    
-    # Train b2 and b3's prediction net
-    prediction.b23_train.main()
-    
-    # Train b2 and b3's residue net
-    residue.b23_train.main()
+#     # Train b2 and b3's residue net
+#     residue.b23_train.main()
 
 ##################### New Method: test ####################
     # Test b1's prediction net
